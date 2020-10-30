e6c5efad89514a7eeb0b97fe683d0f4acc9b1aac
Chloe-Le97 <phuonganhkitty365@gmail.com>
2020-10-30 04:33:02 +0200

    add style to front-end part

---
+++ build/precache-manifest.35809d9ecb928288ffa40555df4b4dfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33b048794f79417536bea2139842372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd9b85bd8855541094bf",
+    "url": "/static/css/main.24a75800.chunk.css"
+  },
+  {
+    "revision": "1fd3e6dd76e039b6c1a3",
+    "url": "/static/js/2.8d094625.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8d094625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9b85bd8855541094bf",
+    "url": "/static/js/main.d6180731.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
